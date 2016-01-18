a69d28fd8d9350458451e1a06c5a85047787cc4e
Ionică Bizău <bizauionica@gmail.com>
2016-01-18 11:26:42 +0200

    Better example

--- example/index.js
+++ example/index.js
@@ -1,88 +1,88 @@
 // Dependencies
-var IsSsh = require("../lib");
+const isSsh = require("../lib");
 
 // Secure Shell Transport Protocol (SSH)
-console.log(IsSsh("ssh://user@host.xz:port/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://user@host.xz:port/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://user@host.xz/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://user@host.xz/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://host.xz:port/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://host.xz:port/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://host.xz/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://host.xz/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://user@host.xz/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://user@host.xz/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://host.xz/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://host.xz/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://user@host.xz/~user/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://user@host.xz/~user/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://host.xz/~user/path/to/repo.git/"));
-// => true
+console.log(isSsh("ssh://host.xz/~user/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("ssh://user@host.xz/~/path/to/repo.git"));
-// => true
+console.log(isSsh("ssh://user@host.xz/~/path/to/repo.git"));
+// true
 
-console.log(IsSsh("ssh://host.xz/~/path/to/repo.git"));
-// => true
-
-
-console.log(IsSsh("user@host.xz:/path/to/repo.git/"));
-// => true
-
-console.log(IsSsh("user@host.xz:~user/path/to/repo.git/"));
-// => true
-
-console.log(IsSsh("user@host.xz:path/to/repo.git"));
-// => true
+console.log(isSsh("ssh://host.xz/~/path/to/repo.git"));
+// true
 
 
-console.log(IsSsh("host.xz:/path/to/repo.git/"));
-// => true
+console.log(isSsh("user@host.xz:/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("host.xz:path/to/repo.git"));
-// => true
+console.log(isSsh("user@host.xz:~user/path/to/repo.git/"));
+// true
 
-console.log(IsSsh("host.xz:~user/path/to/repo.git/"));
-// => true
+console.log(isSsh("user@host.xz:path/to/repo.git"));
+// true
 
 
-console.log(IsSsh("rsync://host.xz/path/to/repo.git/"));
-// => true
+console.log(isSsh("host.xz:/path/to/repo.git/"));
+// true
+
+console.log(isSsh("host.xz:path/to/repo.git"));
+// true
+
+console.log(isSsh("host.xz:~user/path/to/repo.git/"));
+// true
+
+
+console.log(isSsh("rsync://host.xz/path/to/repo.git/"));
+// true
 
 
 // Git Transport Protocol
-console.log(IsSsh("git://host.xz/path/to/repo.git/"));
-// => false
+console.log(isSsh("git://host.xz/path/to/repo.git/"));
+// false
 
-console.log(IsSsh("git://host.xz/~user/path/to/repo.git/"));
-// => false
+console.log(isSsh("git://host.xz/~user/path/to/repo.git/"));
+// false
 
 // HTTP/S Transport Protocol
-console.log(IsSsh("http://host.xz/path/to/repo.git/"));
-// => false
+console.log(isSsh("http://host.xz/path/to/repo.git/"));
+// false
 
-console.log(IsSsh("https://host.xz/path/to/repo.git/"));
-// => false
+console.log(isSsh("https://host.xz/path/to/repo.git/"));
+// false
 
 // Local (Filesystem) Transport Protocol
-console.log(IsSsh("/path/to/repo.git/"));
-// => false
+console.log(isSsh("/path/to/repo.git/"));
+// false
 
-console.log(IsSsh("path/to/repo.git/"));
-// => false
+console.log(isSsh("path/to/repo.git/"));
+// false
 
-console.log(IsSsh("~/path/to/repo.git"));
-// => false
+console.log(isSsh("~/path/to/repo.git"));
+// false
 
-console.log(IsSsh("file:///path/to/repo.git/"));
-// => false
+console.log(isSsh("file:///path/to/repo.git/"));
+// false
 
-console.log(IsSsh("file://~/path/to/repo.git/"));
-// => false
+console.log(isSsh("file://~/path/to/repo.git/"));
+// false
